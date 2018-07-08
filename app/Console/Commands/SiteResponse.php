11878bc5be30c43caacb9a634e6c373eb67b184f
Jakob Buis <jakob@jakobbuis.nl>
2018-07-08 09:49:36 +0200

    port up check from previous project

---
+++ app/Console/Commands/SiteResponse.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Incident;
+use App\Website;
+use GuzzleHttp\Client;
+use Illuminate\Console\Command;
+
+class SiteResponse extends Command
+{
+    protected $signature = 'check:response';
+    protected $description = 'Check websites if they are up or down';
+
+    private $guzzle;
+
+    public function __construct(Client $guzzle)
+    {
+        parent::__construct();
+
+        $this->guzzle = $guzzle;
+    }
+
+    public function handle()
+    {
+        Website::all()->each([$this, 'checkStatus']);
+    }
+
+    public function checkStatus(Website $website)
+    {
+        $response = $this->guzzle->get($website->url, ['http_errors' => false]);
+        $status = $response->getStatusCode();
+
+        if ($status < 300) {
+            return;
+        }
+
+        Incident::firstOrCreate([
+            'type' => 'SiteDown',
+            'website_id' => $website->id,
+            'level' => 1,
+        ]);
+    }
+}
