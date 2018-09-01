8fadad7c61e0790e34b3a66192dd634ee47f326e
Jakob Buis <jakob@jakobbuis.nl>
2018-09-01 17:43:05 +0200

    fix running the scheduler command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('check:up')->everyFiveMinutes();
+        $schedule->command('check:response')->everyFiveMinutes();
     }
 
     /**
