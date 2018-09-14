a812dd621937d8f20af0e331c593b57f667f2455
Jakob Buis <jakob.buis@in10.nl>
2018-09-14 16:59:24 +0200

    restore the schedule to run check:response every 15 minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('check:response')->everyMinute();
+        $schedule->command('check:response')->everyFifteenMinutes();
     }
 
     /**
