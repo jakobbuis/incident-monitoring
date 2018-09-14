547e0ec3f059b1c92e7a3bc5bfa6776c066bf443
Jakob Buis <jakob.buis@in10.nl>
2018-09-14 16:59:10 +0200

    try every check:response check three times before declaring an incident, to reduce fluttering

--- app/Console/Commands/SiteResponse.php
+++ app/Console/Commands/SiteResponse.php
@@ -29,12 +29,71 @@ class SiteResponse extends Command
         $this->resolveIncident = $resolveIncident;
     }
 
+    /**
+     * Try every website 3 times to give a valid response, trigger an incident
+     * when all three checks fail
+     */
     public function handle()
     {
-        Website::all()->each([$this, 'checkStatus']);
+        $websites = Website::all();
+
+        // Check all websites
+        $websites = $websites->filter(function ($website) {
+            return !$this->check($website);
+        });
+
+        // Check all failing websites again after a short delay
+        sleep(5);
+        $websites = $websites->filter(function ($website) {
+            return !$this->check($website);
+        });
+
+        // Check all failing websites again after a short delay
+        // create an incident if this third check fails as well
+        sleep(5);
+        $websites->each(function ($website) {
+            $this->checkAndStartIncident($website);
+        });
     }
 
-    public function checkStatus(Website $website)
+    /**
+     * Check a URL for a valid response
+     * @param  string $url
+     * @return bool
+     */
+    private function check(Website $website) : bool
+    {
+        // Prevent a RuntimeError by aliasing to local scope variables
+        $resolveIncident = $this->resolveIncident;
+
+        try {
+            $response = $this->guzzle->get($website->url, ['http_errors' => false]);
+            $status = $response->getStatusCode();
+        } catch (GuzzleException $e) {
+            Log::info("Website {$website->name} responded to check with GuzzleException", [
+                'exception' => $e,
+            ]);
+            return false;
+        }
+
+        Log::info("Website {$website->name} responded to check with HTTP {$status}");
+
+        if ($status < 400) {
+            $resolveIncident($website, 'SiteDown');
+            $resolveIncident($website, 'CertificateError');
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Check a website for a valid response, starting an incident if the website
+     * fails this third check again
+     * @param  Website $website
+     * @return void
+     */
+    private function checkAndStartIncident(Website $website) : void
     {
         // Prevent a RuntimeError by aliasing to local scope variables
         $start = $this->startIncident;
@@ -59,7 +118,7 @@ class SiteResponse extends Command
         }
         Log::info("Website {$website->name} responded to check with HTTP {$status}");
 
-        if ($status >= 400) {
+        if ($$status >= 400) {
             $start($website, 'SiteDown', Incident::LEVEL_CRITICAL, [
                 'http_status_code' => $status,
             ]);
