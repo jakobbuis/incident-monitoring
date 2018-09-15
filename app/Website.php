237fd13605336e46b310af41fdc03cdf9672784c
Jakob Buis <jakob@jakobbuis.nl>
2018-09-15 14:27:48 +0200

    Revert "Merge branch 'feature/backpack-admin'"
    This reverts commit 97fad30bedafa2b158d91772fda30bf84206b65a, reversing
    changes made to 276b71bf076bdc316b89422033b4ddb94900781f.

--- app/Website.php
+++ app/Website.php
@@ -3,12 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Backpack\CRUD\CrudTrait;
 
 class Website extends Model
 {
-    use CrudTrait;
-
     protected $fillable = ['name', 'url'];
 
     public function incidents()
