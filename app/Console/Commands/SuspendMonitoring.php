4001adbf13a978e75f57c4955d387d6d3843848b
Jakob Buis <jakob@jakobbuis.nl>
2019-03-24 12:53:25 +0100

    enable suspending websites by partial name

--- app/Console/Commands/SuspendMonitoring.php
+++ app/Console/Commands/SuspendMonitoring.php
@@ -8,21 +8,33 @@ use Illuminate\Console\Command;
 
 class SuspendMonitoring extends Command
 {
-    protected $signature = 'website:suspend {id}';
+    protected $signature = 'website:suspend';
     protected $description = 'Suspend the monitoring for a website';
 
     public function handle()
     {
         // Find the website
-        $id = $this->argument('id');
-        $website = Website::find($id);
-        if (!$website) {
-            $this->error("Cannot find website with ID {$id}");
+        $name = $this->ask('Which website should we suspend?');
+        $candidates = Website::where('name', 'LIKE', "%{$name}%")->get();
+        if ($candidates->count() === 0) {
+            $this->info("Cannot find any website with '{$name}' in its name");
             return;
+        } elseif ($candidates->count() === 1) {
+            $website = $candidates->first();
+            if (!$this->confirm("Suspend {$website->name}?", true)) {
+                return;
+            }
+        } else {
+            $name = $this->choice('Which website?', $candidates->pluck('name')->toArray());
+            $website = $candidates->first(function($candidate) use ($name) {
+                return $candidate->name === $name;
+            });
         }
 
         // Suspend the website
         $website->monitoring_suspended = Carbon::now();
         $website->save();
+
+        $this->info("Suspended monitoring for {$website->name}");
     }
 }
